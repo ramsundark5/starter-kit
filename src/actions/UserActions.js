c769ba3059fbff9d9b8dd8669bc377e5eeceea50
Ramsundar Kuppusamy <ramsundar@Ramsundars-MBP.T-mobile.com>
2017-10-09 15:37:14 -0700

    setup circleci

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,16 +1,5 @@
 import * as AppConstants from '../constants/AppConstants'
-import { CALL_API, Schemas } from '../middleware/api'
 import {firebaseAuth} from '../security/FirebaseConfig'
-/* 
-// Fetches a single user from Github API.
-// Relies on the custom API middleware defined in ../middleware/api.js.
-const fetchUser = loginId => ({
-  [CALL_API]: {
-    types: [ AppConstants.USER_LOGGING_IN, AppConstants.USER_LOGGED_IN, AppConstants.USER_LOGGED_OUT ],
-    endpoint: `users/${loginId}`,
-    schema: Schemas.USER
-  }
-}) */
 
 export function login(loggedInUser) {
   return {
@@ -26,14 +15,3 @@ export async function logout(){
     payload: null
   }
 }
-
-/* // Fetches a single user from Github API unless it is cached.
-// Relies on Redux Thunk middleware.
-export const loadUser = (loginId, requiredFields = []) => (dispatch, getState) => {
-  const user = getState().entities.users[login]
-  if (user && requiredFields.every(key => user.hasOwnProperty(key))) {
-    return null
-  }
-
-  return dispatch(fetchUser(loginId))
-} */
